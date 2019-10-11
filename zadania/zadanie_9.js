2c6db291dd88e768064df1a83ee23138f495cbec
Maix19bep <maix19bep@gmail.com>
2019-10-11 17:55:18 +0200

    Zadanie9: created function

---
+++ zadania/zadanie_9.js
@@ -0,0 +1,15 @@
+const numbers = [1, 5, 6, 8, 2, 4, 98, 24, 67];
+
+function findNumbers(array) {
+  let foundNumbers = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (numbers.indexOf(array[i]) !== -1) {
+      foundNumbers.push(array[i]);
+    }
+  }
+
+  return foundNumbers;
+}
+
+console.log(findNumbers([4, 8, 11, 24, 100]));
