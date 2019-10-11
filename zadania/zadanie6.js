70f41199f7b813084e15f0b371b9799e2fdc05d7
Maix19bep <maix19bep@gmail.com>
2019-10-11 18:20:38 +0200

    zadanie_6: not resolved

---
+++ zadania/zadanie6.js
@@ -0,0 +1,7 @@
+/* Stwórz funkcję, która jako parametr przyjmie liczbę, usunie ją z tablicy, zwróć tablice po usunieciu */
+
+let tablica2 = [7, 2, 3];
+
+console.log('przed ', tablica2);
+tablica2.slice(1, 1);
+console.log('po ', tablica2);
