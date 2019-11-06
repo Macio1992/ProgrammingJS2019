9400bbc1d758db917ff60cd4773fa8fed267d553
Maix19bep <maix19bep@gmail.com>
2019-11-06 19:44:18 +0100

    zadanie6: const

--- zadania/zadanie6.js
+++ zadania/zadanie6.js
@@ -1,6 +1,6 @@
 /* Stwórz funkcję, która jako parametr przyjmie liczbę, usunie ją z tablicy, zwróć tablice po usunieciu */
 
-let array = [4, 5, 6, 7, 1, 3, 45, 67];
+const array = [4, 5, 6, 7, 1, 3, 45, 67];
 
 function removeElement(indexToRemove, deleteCount) {
   array.splice(indexToRemove, deleteCount);
