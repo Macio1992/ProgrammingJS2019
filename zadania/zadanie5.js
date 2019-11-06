eacf81fd68ed79ed7480a97cb11244c87d6831c0
Maix19bep <maix19bep@gmail.com>
2019-11-06 18:39:26 +0100

    zadanie5: Fixes 2

--- zadania/zadanie5.js
+++ zadania/zadanie5.js
@@ -1,5 +1,4 @@
 function getRandomDigit(array) {
   let randomDigitIndex = Math.floor((Math.random() * (array.length - 1)) + 0);
-  console.log('randomIndex ', randomDigitIndex);
   return array[randomDigitIndex];
 }
