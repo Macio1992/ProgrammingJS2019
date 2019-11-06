52b52ddc84994de465928354111254e091743924
Maix19bep <maix19bep@gmail.com>
2019-11-06 19:39:33 +0100

    zadanie6: Finally Done

--- zadania/zadanie6.js
+++ zadania/zadanie6.js
@@ -1,7 +1,13 @@
 /* Stwórz funkcję, która jako parametr przyjmie liczbę, usunie ją z tablicy, zwróć tablice po usunieciu */
 
-let tablica2 = [7, 2, 3];
+let tablica = [4, 5, 6, 7, 1, 3, 45, 67];
 
-console.log('przed ', tablica2);
-tablica2.slice(1, 1);
-console.log('po ', tablica2);
+console.log(tablica);
+
+function removeElement(indexToRemove, deleteCount) {
+  tablica.splice(indexToRemove, deleteCount);
+}
+
+removeElement(4, 1);
+
+console.log(tablica);
