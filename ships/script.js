50fbba4129960afaf53a23bfbce73e17a4e306dd
Maix19bep <maix19bep@gmail.com>
2019-11-26 18:45:45 +0100

    task: changes

--- ships/script.js
+++ ships/script.js
@@ -1,24 +1,22 @@
 function hit(event) {
-  // if (event.target.classList.contains("icon")) {
-  //   console.log("trafiony");
-  //   event.target.classList.remove("ship");
-  //   event.target.classList.add("hitShip");
-  // } else {
-  //   console.log("nie trafiony");
-  //   event.target.classList.add("skull");
-  //   event.target.classList.add("icon");
-  // }
 
-  const cellShips = document.getElementsByClassName("ship");
-  // console.log("cellShips", cellShips);
+}
 
-  let fields = [];
+function getRandomSets() {
+  let allowedSets = [
+    ['A1', 'B3', 'C1'],
+    ['A3', 'B1', 'C3'],
+    ['A2', 'C1', 'C3']
+  ];
+  const randomIndex = Math.floor((Math.random() * (allowedSets.length - 1)) + 0);
 
-  for (let i = 0; i < cellShips.length; i++) {
-    fields.push(cellShips[i].classList[0]);
+  return allowedSets[randomIndex];
+}
+
+function fillBoardWithShips() {
+  const randomSets = getRandomSets();
+
+  for (let i = 0; i < randomSets.length; i++) {
+    
   }
-  console.log(fields);
-
-  // const hitCell = event.target;
-  // console.log(hitCell.classList[0]);
 }
