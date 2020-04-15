be180934ee15296a9c36bac091f5831448eee35c
ProgrammerGdansk2019 <kuba4live@gmail.com>
2020-04-15 18:09:03 +0200

    zadanie2: paragraf added

--- Game/script.js
+++ Game/script.js
@@ -2,4 +2,7 @@ let element = document.getElementById("jeden")
 
 function changeColor() {
     element.classList.add("red")
+    let paragraf = document.getElementById("paragraf")
+    paragraf.innerHTML = 'squareClicked'
 }
+
