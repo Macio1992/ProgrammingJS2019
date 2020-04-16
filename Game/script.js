21369c223127b9a06273005a58cddf6eccaa4093
ProgrammerGdansk2019 <kuba4live@gmail.com>
2020-04-16 08:56:28 +0200

    Zadanie3: Done

--- Game/script.js
+++ Game/script.js
@@ -1,8 +1,11 @@
+let counter = 0
 
 function changeColor() {
     let element = document.getElementById('jeden');
     element.classList.add('red');
     let paragraf = document.getElementById('paragraph');
     paragraf.innerHTML = 'squareClicked';
-}
-
+    let licznik = document.getElementById('counter');
+    counter++;
+    licznik.innerHTML = 'Kliknieto ' + counter;
+};
