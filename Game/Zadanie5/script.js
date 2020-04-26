88f25ebb797871ebbf2810c7ad270330da7a0dc5
ProgrammerGdansk2019 <kuba4live@gmail.com>
2020-04-26 11:10:14 +0200

    Zadanie6: colors

--- Game/Zadanie5/script.js
+++ Game/Zadanie5/script.js
@@ -1,6 +1,9 @@
 let chosenIds = [];
+let colors = ["red", "green", "blue"];
 
-function sendID(id) {
+function sendID(id, index) {
+    let element = document.getElementById(id);
     chosenIds.push(id);
-    console.log(chosenIds);
+    let color = colors[index-1];
+    element.classList.add(color);
 }
