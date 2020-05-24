d3d563d0a79035a22bbeb6fb2a4a680735b9dacd
ProgrammerGdansk2019 <kuba4live@gmail.com>
2020-05-24 11:31:27 +0200

    FC_!: Fixes

--- find-color/script.js
+++ find-color/script.js
@@ -1,9 +1,10 @@
+const QUESTION_MARK_CLASS = "question_mark";
 const colors = ['blue', 'red', 'green', 'black', 'yellow'];
 const colorToFind = colors[2];
 
-
 function guessColor(elementId, index) {
     const clickedElement = document.getElementById(elementId);
-    clickedElement.classList.add(colors[index-1])
-    clickedElement.classList.remove("question_mark")
+    const clickedColor = colors[index-1];
+    clickedElement.classList.add(clickedElement);
+    clickedElement.classList.remove(QUESTION_MARK_CLASS);
 }
