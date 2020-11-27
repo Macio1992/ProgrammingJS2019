f4afae5d9322059ae8fc17afd6c4f1250c75d691
ProgrammerGdansk2019 <kuba4live@gmail.com>
2020-11-27 18:31:52 +0100

    JD_8: Done

---
+++ JS2020/JS_8.js
@@ -0,0 +1,21 @@
+
+let Numbers = {
+    1: "circle",
+    2: "semi-circle",
+    3: "triangle",
+    4: "square",
+    5: "pentagon",
+    6: "hexagon",
+    7: "heptagon",
+    8: "octagon",
+    9: "nonagon",
+    10: "decagon",
+}
+
+function nSidedShape(n) {
+    return Numbers[n];
+}
+
+console.log(nSidedShape(3));
+console.log(nSidedShape(1));
+console.log(nSidedShape(9));
